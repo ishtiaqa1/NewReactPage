e43efa83b3530f5694f645df4ce92cad3386762b
ishtiaq <ishtiaqa@buffalo.edu>
2025-11-19 22:34:21 -0500

    Added git contribution

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './About.css';
+import { GitHubCalendar } from "react-github-calendar";
 
 const About = () => {
   return (
-    <div>
+    <div className='about-container'>
       <h1>Hi! Im Ishtiaq Akanda</h1>
       <h2>
         F'25 Software Engineering Fellow at The Marcy Lab School. Passionate about games, anime, sports, and of course the outside world time to time.
       </h2>
 
-      <h2>Languages</h2>
+      <h2>My GitHub Contributions</h2>
+      <GitHubCalendar username="ishtiaqa1" />
+
+      <h2>Programming Languages</h2>
       <ul>
         <li>Python</li>
-        <li>Javascript</li>
+        <li>JavaScript</li>
         <li>Scala</li>
         <li>SQL</li>
         <li>PHP</li>
@@ -23,8 +27,8 @@ const About = () => {
 
       <h2>Frameworks/Tools</h2>
       <ul>
-        <li>React.JS</li>
-        <li>Node.JS</li>
+        <li>React.js</li>
+        <li>Node.js</li>
         <li>Linux</li>
         <li>Git</li>
         <li>Github</li>
@@ -36,7 +40,7 @@ const About = () => {
       <h2>Data Tools</h2>
       <ul>
         <li>Stata</li>
-        <li>Tableu</li>
+        <li>Tableau</li>
         <li>IBM Watson</li>
         <li>MySQL</li>
         <li>phpMyAdmin</li>
