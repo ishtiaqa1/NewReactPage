1252df2a6f0dfba2e565a7604f344b3413c03ae0
ishtiaq <ishtiaqa@buffalo.edu>
2025-11-19 21:11:37 -0500

    Made the home page more responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <nav className='projects'>
+      <div className='projects-container'>
+      <ul>
+        <li className='projectElement'>Mahjong App</li>
+        <li className='projectElement'></li>
+        <li className='projectElement'></li>
+        <li className='projectElement'></li>
+      </ul>
+      </div>
+    </nav>
   )
 }
 
