9683fbe6f19153473ccc48dbffc495e92aa568ec
Ishtiaq Akanda <95101462+ishtiaqa1@users.noreply.github.com>
2026-01-29 01:33:49 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,10 +33,10 @@ const Contact = () => {
           </section>
           <section className="text-box">
             <label for="email">E-Mail</label>
-            <input id="email" type="text" required placeholder="Yourmail@example.com"></input>
+            <input id="email" type="email" required placeholder="Yourmail@example.com"></input>
           </section>
           <section className="text-area">
-            <label for="iquiry">Inquiry</label>
+            <label for="inquiry">Inquiry</label>
             <textarea id="inquiry" required placeholder="Messages, requests, and more!"></textarea>
           </section>
           <button className="btn btn-primary" type="submit">Submit</button>
